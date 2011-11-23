853aee3ae22398ba597ce46e6b4c3d40460deaac
Michael Manring <mrmanrin@ncsu.edu>
2011-11-23 16:32:09 -0500

    Repository initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Singularity::Application.config.secret_token = '8cee0053a660f798713ad7ae3fef7f24388d6ac4ae0db2fcb946a7a22d42fe1d18f61c59ff787922d3b987295dde5e17c01b19dea5b126b71674a27dbd04432a'
